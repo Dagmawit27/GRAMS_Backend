6580f226770640ba92317992d0e0853ab6c828bf
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-08-03 15:48:56 +0300

    feat: implement Role and UserRole entities with relationships and UUID generation

--- backend/src/main/java/com/ethiorental/backend/IAM/entity/Role.java
+++ backend/src/main/java/com/ethiorental/backend/IAM/entity/Role.java
@@ -1,5 +1,26 @@
 package com.ethiorental.backend.IAM.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.*;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
 public class Role {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
 }
--- backend/src/main/java/com/ethiorental/backend/IAM/entity/UserRole.java
+++ backend/src/main/java/com/ethiorental/backend/IAM/entity/UserRole.java
@@ -1,5 +1,38 @@
 package com.ethiorental.backend.IAM.entity;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user_roles",
+
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "role_id"})
+    }
+)
 public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
     
 }
